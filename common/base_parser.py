5b130ee6dffc48a3e3473424cd7ecc5bd66867e0
MathiasLuo <mathiasluo@gmail.com>
2016-07-21 11:26:55 +0800

    add arg parse help

--- common/base_parser.py
+++ common/base_parser.py
@@ -1,8 +1,8 @@
 from flask_restful import reqparse
 
 baseParser = reqparse.RequestParser()
-baseParser.add_argument('url', type=str, required=True)
+baseParser.add_argument('url', type=str, required=True, help='this is the query url')
 
 urlParse = baseParser.copy()
-urlParse.add_argument('format')
-urlParse.add_argument('time', type=bool)
+urlParse.add_argument('format', help='this is the video format')
+urlParse.add_argument('time', type=bool, help='this arg is you want to return time arg')
