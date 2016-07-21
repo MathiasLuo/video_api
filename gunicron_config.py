366d7176f0dbcd77a6e44dd0755e4b024b560d74
MathiasLuo <mathiasluo@gmail.com>
2016-07-21 11:30:45 +0800

    add gunicron_config.py

--- gunicron_config.py
+++ gunicron_config.py
@@ -1,8 +1,14 @@
+import os
 
 bind = '127.0.0.1:5000'  # 绑定的端口
 workers = 1  # worker数量
 backlog = 2048
+debug = True
 timeout = 30
+accesslog = '/var/log/gunicorn/access.log'
+errorlog = '/var/log/gunicorn/error.log'
+proc_name = 'gunicorn.pid'
+pidfile = '/var/log/gunicorn/debug.log'
 
 
 def worker_abort(worker):
